d0180749a6576f3349c4173ab37177d8b78c9d3c
Candela Ávila Costa <91270994+candelavila@users.noreply.github.com>
2021-12-22 02:13:39 -0300

    Update Player.h
    Agregué la declaración de score, el texto del mismo y la fuente con la que se escribiría en pantalla.

--- agregado de game over y obstáculos/Player.h
+++ agregado de game over y obstáculos/Player.h
@@ -11,6 +11,11 @@ public:
 private:
 	Vector2f m_pos,m_speed;
 	int m_num_frame = 0;
+	
+	//para el score
+	int score = 0; //valor por defecto xq se reinicia el juego
+	Font m_font;
+	Text m_text_score;
 };
 
 #endif
