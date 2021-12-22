14dea7f623c323d54c92f79fc3ed7b5d726139f7
Candela Ávila Costa <91270994+candelavila@users.noreply.github.com>
2021-12-22 02:08:32 -0300

    Update Player.cpp
    La variable score va acumulando los puntos del Player a medida que esquiva obstáculos.
    En un primer momento pensé en usar la variable en la clase Match, pero la termina agregando a la clase Player.
    En el método Update( ) pasé por parámetro el tiempo del juego transcurrido y después lo usé en segundos, para que vaya aumentando el score.
    Tengo que ver si elapsed se tendría que multiplicar por otra variable distinta a m_speed.x.

--- agregado de game over y obstáculos/Player.cpp
+++ agregado de game over y obstáculos/Player.cpp
@@ -3,10 +3,18 @@
 
 Player::Player() : Objeto ("walk.png") , m_pos(270,304){
 	m_s.scale(0.5,0.5);
+	
+	// agregado del texto para el acumulador de puntos
+	m_font.loadFromFile("FreePixel.ttf");
+	m_text_score.setFont(m_font);
+	m_text_score.setString(score + " m");
+	m_text_score.setFillColor({150,150,150});
+	m_text_score.setPosition(600 ,100);
+	m_text_score.setCharacterSize(20);
 }
 
 
-void Player::Update ( ) {
+void Player::Update (Time elapsed ) {
 
 	auto tex_sz = m_t.getSize();
 	m_s.setTextureRect({(m_num_frame/10)*int(tex_sz.x)/4,0,int(tex_sz.x)/4,int(tex_sz.y)});
@@ -16,6 +24,9 @@ void Player::Update ( ) {
 	
 	m_speed.y += 0.15;
 	m_pos += m_speed;
+	
+	score += m_speed.x * elapsed.asSeconds() ; //el score aumenta a medida que pasa el tiempo
+	m_text_score.setString(score + " m");
 	
 	if(m_pos.y>=304){
 		m_speed.y = 0;
