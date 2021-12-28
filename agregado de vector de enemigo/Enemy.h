a842410212521e58b02f0298155d1d4dc0a2b046
Masacremc <90012960+Masacremc@users.noreply.github.com>
2021-12-28 14:35:29 -0300

    Add files via upload

---
+++ agregado de vector de enemigo/Enemy.h
@@ -0,0 +1,19 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+#include "ObjetoSinTex.h"
+#include "Enemy.h"
+
+class Enemy : public ObjetoSinTex {
+private: 
+	int posx=0;
+	int posy=0;
+	int m_speed=-5;
+public:
+	Enemy(sf::Texture &t);
+	void Update();
+	Vector2f SIZE();
+	void CambiarPosicion (int pos_y,int i);
+};
+
+#endif
+
---
+++ agregado de vector de enemigo/ObjetoSinTex.h
@@ -0,0 +1,20 @@
+#ifndef OBJETO_SIN_TEX_H
+#define OBJETO_SIN_TEX_H
+#include <string>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+class ObjetoSinTex {
+public:
+	ObjetoSinTex(sf::Texture &t);
+	void Draw(RenderWindow &ww);
+	FloatRect GetBB()const;
+protected:
+	Sprite m_s;
+};
+
+#endif
