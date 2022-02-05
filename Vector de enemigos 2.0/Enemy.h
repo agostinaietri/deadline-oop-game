c3b8e692746073f2b92055dc81eb5952a931a90c
Masacremc <90012960+Masacremc@users.noreply.github.com>
2022-02-04 23:51:51 -0300

    Add files via upload

---
+++ Vector de enemigos 2.0/Enemy.h
@@ -0,0 +1,19 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+#include "ObjetoSinTex.h"
+#include "Enemy.h"
+
+class Enemy : public ObjetoSinTex {
+private: 
+	int posx=0;
+	int posy=0;
+	float m_speed;
+public:
+	Enemy(sf::Texture &t,float speed);
+	void Update();
+	Vector2f posicion();
+	void CambiarPosicion (int pos_y,int i);
+};
+
+#endif
+
---
+++ Vector de enemigos 2.0/Scene.h
@@ -0,0 +1,19 @@
+#ifndef SCENE_H
+#define SCENE_H
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "Game.h"
+
+
+using namespace sf;
+class Game;
+class Scene {
+public:
+	Scene();
+	virtual void Draw(RenderWindow &Window) = 0;
+	virtual void Update(Game &gamee) = 0;
+	virtual ~Scene () {};
+private:
+};
+
+#endif
+
