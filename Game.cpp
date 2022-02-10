56d45e4a1037d56d53f2503e1e103b7833205376
Agostina Ietri <58744117+agostinaietri@users.noreply.github.com>
2022-02-10 12:02:06 -0300

    Add files via upload

---
+++ Game.cpp
@@ -0,0 +1,50 @@
+#include "Game.h"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics.hpp>
+#include "Menu.h"
+#include "Scene.h"
+
+using namespace sf;
+
+Game::Game() : m_w(VideoMode(720,480),"deadline"){
+	m_w.setFramerateLimit(60);
+	m_scene = new Menu();
+}
+
+
+void Game::Run ( ) {
+	while(m_w.isOpen()) {
+		ProcesadoJuego();
+		Update();
+		Draw();
+		if(m_next_scene){
+			delete m_scene;
+			m_scene = m_next_scene;	///eliminando la escena acá te evitas problemas de que reviente el juego
+			m_next_scene = nullptr;
+		}
+	}
+}
+
+
+
+void Game::Draw ( ) {
+	m_scene->Draw(m_w);
+}
+
+void Game::Update () {
+	m_scene->Update(*this);
+}
+
+void Game::ProcesadoJuego ( ) {
+	Event e;
+	while(m_w.pollEvent(e)){ {
+		if(e.type == Event::Closed)
+			m_w.close();	
+		}
+	}
+}
+
+void Game::SetScene (Scene * next_scene) {
+	m_next_scene = next_scene;
+}
+
---
+++ Player.cpp
@@ -0,0 +1,33 @@
+#include "Player.h"
+#include <SFML/Window/Keyboard.hpp>
+
+Player::Player() : Objeto ("walk.png") , m_pos(270,304){
+	m_s.scale(0.5,0.5);
+}
+
+//Player::Player(String m_img) : Objeto("shadow.png"), m_pos(-200,48) {
+//	m_s.setScale(2.0,2.0);
+//};
+
+void Player::Update ( ) {
+
+	auto tex_sz = m_t.getSize();
+	m_s.setTextureRect({(m_num_frame/10)*int(tex_sz.x)/4,0,int(tex_sz.x)/4,int(tex_sz.y)});
+	m_num_frame = (m_num_frame+1)%40;
+	
+	
+	
+	m_speed.y += 0.15;
+	m_pos += m_speed;
+	
+	if(m_pos.y>=304){
+		m_speed.y = 0;
+		m_pos.y = 304;
+		if(Keyboard::isKeyPressed(Keyboard::Key::Up)){
+			m_speed.y = -6;
+		}
+	}
+	
+	m_s.setPosition(m_pos);
+}
+
